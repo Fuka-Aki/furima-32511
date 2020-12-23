5bd9f652a2d53fb6008fbca93ce9c173e005e587
Fuka-Aki <f.akiyama0926@gmail.com>
2020-12-23 15:33:59 +0900

    Rubocop実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(mix_case: true)}
-    password_confirmation {password}
-    last_name             {"野田"}
-    first_name            {"武雄"}
-    last_name_reading     {"ノダ"}
-    first_name_reading    {"タケオ"}
-    birthday              {"1996-09-26"}
+    nickname              { Faker::Name.initials }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(mix_case: true) }
+    password_confirmation { password }
+    last_name             { '野田' }
+    first_name            { '武雄' }
+    last_name_reading     { 'ノダ' }
+    first_name_reading    { 'タケオ' }
+    birthday              { '1996-09-26' }
   end
 end
