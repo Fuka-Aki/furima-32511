defc1eb6f5ca8a1eb7b10e89441adf9536c0507f
Fuka-Aki <f.akiyama0926@gmail.com>
2020-12-23 10:42:29 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :password,              format: { with: /[a-z\d]{6,}/ }
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,           format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :first_name,          format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :last_name_reading,   format: { with: /\A[ァ-ンヴー]+\z/ }
+    validates :first_name_reading,  format: { with: /\A[ァ-ンヴー]+\z/ }
+    validates :birthday
+  end
 end
