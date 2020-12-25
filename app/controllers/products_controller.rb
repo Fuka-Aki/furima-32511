3816933f671c2b35d87eaf55a4b41de41cd11670
Fuka-Aki <f.akiyama0926@gmail.com>
2020-12-25 18:26:02 +0900

    商品出品機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,31 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit]
+
   def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.valid?
+      @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def authenticate_user!
+    redirect_to root_path unless user_signed_in?
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :product_condition_id, :shopping_charge_id,
+                                    :prefecture_id, :shopping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/product.rb
@@ -0,0 +1,25 @@
+class Product < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id,          numericality: { other_than: 0, message: "can't be blank" }
+    validates :product_condition_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :shopping_charge_id,   numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id,        numericality: { other_than: 0, message: "can't be blank" }
+    validates :shopping_day_id,      numericality: { other_than: 0, message: "can't be blank" }
+    validates :price,                numericality: { only_integer: true, message: "is invalid" }
+    validates :user
+  end
+  
+  belongs_to :user, foreign_key: 'user_id'
+  has_one_attached :image
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :shopping_charge
+  belongs_to :prefecture
+  belongs_to :shopping_day
+end
