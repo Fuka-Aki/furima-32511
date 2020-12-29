2eb21bbe733eb73d77ce04cf63cc37f1ca84a6f3
Fuka-Aki <f.akiyama0926@gmail.com>
2020-12-29 11:53:30 +0900

    product_controller手直し

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+<<<<<<< Updated upstream
   before_action :move_to_index, only: [:edit]
+=======
+  before_action :move_to_index, only: [:edit]	
+>>>>>>> Stashed changes
   before_action :set_product, only: [:show, :edit]
 
   def index
@@ -35,19 +39,31 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
+  def edit	
+  end	
 
+  def update	
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
+  private
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :product_condition_id, :shopping_charge_id,
                                     :prefecture_id, :shopping_day_id, :price, :image).merge(user_id: current_user.id)
   end
   def move_to_index
     @product = Product.find(params[:id])
-    redirect_to action: :index unless current_user.id == @product.user_id
+    unless current_user.id == @product.user_id?
+      redirect_to action: :index
+    end
   end
 
   def set_product
     @product = Product.find(params[:id])
   end
-
 end
